03241ce1c29f01aaaeebdb37025e8c9d6acdcafe
serjbarhatov <barh0001@hz.nl>
2025-02-16 17:17:17 +0100

    Create faqs table migration

--- database/migrations/2025_02_16_161221_create_faqs_table.php
+++ database/migrations/2025_02_16_161221_create_faqs_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('faqs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title')->unique();
-            $table->string('slug')->unique();
-
-            $table->longText('body');
+            $table->string('question')->unique();
+            $table->string('answer');
+            $table->string('link')->nullable();
         });
     }
 
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('faqs');
     }
 };
